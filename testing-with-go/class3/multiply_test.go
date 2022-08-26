2e867f174ff10e42478612777baea47fa99834df
chrisloarryn <chrisloarryn@gmail.com>
2022-08-25 20:21:40 -0400

    hola

--- testing-with-go/class3/multiply_test.go
+++ testing-with-go/class3/multiply_test.go
@@ -4,23 +4,29 @@ import "testing"
 
 func TestMultiply(t *testing.T) {
 	table := []struct {
+		name string
 		a    int
 		b    int
 		want int
 	}{
-		{2, 1, 2},
-		{2, 2, 4},
-		{2, 3, 6},
-		{2, 4, 8},
-		{2, 5, 10},
+		{"2x1", 2, 1, 2},
+		{"2x2", 2, 2, 4},
+		{"2x3", 2, 3, 6},
+		{"2x4", 2, 4, 8},
+		{"2x5", 2, 5, 10},
 	}
 
-	for _, v := range table {
-		got := multiply(v.a, v.b)
+	// TODO: executing a single test in the table
+	// to execute subtest "go test -v -run 2x3"
 
-		if got != v.want {
-			t.Errorf("Expected result (%d), actual result (%d)", v.want, got)
-		}
+	for _, v := range table {
+		t.Run(v.name, func(t *testing.T) {
+			got := multiply(v.a, v.b)
+
+			if got != v.want {
+				t.Errorf("Expected result (%d), actual result (%d)", v.want, got)
+			}
+		})
 	}
 
 }
